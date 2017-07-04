2bc0b62853c38290c46fc20ff9b423e494bc54d6
jatin kumar vimal <jatin kumar vimal>
2017-07-04 11:14:57 +0530

    Home Page Completely Change

--- src/HomePage.java
+++ src/HomePage.java
@@ -1,63 +1,130 @@
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
 
 /**
- * Created by jatin kumar vimal on 30/06/2017.
+ * Created by jatin kumar vimal on 04/07/2017.
  */
 public class HomePage {
-    JPanel panelHome;
-    JButton loginbutton,Exitbutton,registrationButton;
-    JLabel Homelabel;
-    public HomePage(JFrame jFrame ) throws IOException {
+    JPanel panelMiddle;
+
+    JButton loginbutton;
+    JLabel Homelabel,aboutuslabel,policylabel,faqslabel,contactlabel;
+    JLabel Hotellabel,hollalabel,textlabel;
+
+    public HomePage(JFrame jFrame) throws IOException {
         jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
         jFrame.setTitle("Home Page");
 
-        loginbutton = new JButton("LOG IN");
-        Exitbutton = new JButton("EXIT");
-        registrationButton= new JButton("Registration ");
-        Homelabel= new JLabel("WELCOME TO OUR HOTEL");
-        Homelabel.setFont(new Font("Times New Roman",Font.BOLD,20));
-        Homelabel.setBounds(120,120,300,25);
-        loginbutton.setBounds(180,180,150,25);
-        Exitbutton.setBounds(180,210,150,25);
-        registrationButton.setBounds(180,240,150,25);
-        panelHome= new JPanel();
-        jFrame.add(panelHome);
+        jFrame.setContentPane(new JLabel((new ImageIcon("src\\MainPackage\\images\\123.jpg"))));
+//        loginbutton = new JButton("LOG IN");
+//        Exitbutton = new JButton("EXIT");
+//        registrationButton = new JButton("Registration ");
+//        Homelabel = new JLabel("WELCOME TO OUR HOTEL");
+//        Homelabel.setFont(new Font("Times New Roman", Font.BOLD, 20));
+//        Homelabel.setBounds(120, 120, 300, 25);
+//        loginbutton.setBounds(180, 180, 150, 25);
+//        Exitbutton.setBounds(180, 210, 150, 25);
+//        registrationButton.setBounds(180, 240, 150, 25);
+//        panelHome = new JPanel();
+//        jFrame.add(panelHome);
+//
+//        //  panelHome.setBackground(Color.blue);
+////        panelHome.setBackground(Color.black);
+//        panelHome.setLayout(null);
+//        panelHome.add(Homelabel);
+//        panelHome.add(loginbutton);
+//        panelHome.add(Exitbutton);
+//        panelHome.add(registrationButton);
+//        panelHome.requestFocus();
+//        loginbutton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                panelHome.setVisible(false);
+//                //  new LogInPage(jFrame);
+//            }
+//        });
+//        registrationButton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                panelHome.setVisible(false);
+//                // new RegstrationPage(jFrame);
+//            }
+//        });
+//        Exitbutton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                jFrame.dispose();
+//            }
+//        });
+//        jFrame.setContentPane(new JLabel((new ImageIcon("src\\MainPackage\\images\\holla1.jpg"))));
+        loginbutton= new JButton("BOOK NOW");
+        //panelHome= new JPanel();
+        panelMiddle = new JPanel();
+        Homelabel= new JLabel("Home");
+        aboutuslabel= new JLabel("About Us");
+        policylabel=new JLabel("Policy");
+        faqslabel= new JLabel("FAQS");
+        contactlabel= new JLabel("Contact");
+        Hotellabel = new JLabel("HOTEL");
+        hollalabel=new JLabel("holla");
+        textlabel= new JLabel("(A Place Where You Came As A Guest and Leave As a Family)");
+        //panelMiddle.setLayout(null);
+        panelMiddle.setSize(1200,680);
+        //Hotellabel.setSize(500,400);
+        // panelHome.setSize(1200,150);
+        //  panelHome.setOpaque(false);
+        panelMiddle.setOpaque(false);
+        panelMiddle.setLayout(null);
 
-      //  panelHome.setBackground(Color.blue);
-//        panelHome.setBackground(Color.black);
-        panelHome.setLayout(null);
-        panelHome.add(Homelabel);
-        panelHome.add(loginbutton);
-        panelHome.add(Exitbutton);
-        panelHome.add(registrationButton);
-        panelHome.requestFocus();
-       loginbutton.addActionListener(new ActionListener() {
-           @Override
-           public void actionPerformed(ActionEvent e) {
-               panelHome.setVisible(false);
-               new LogInPage(jFrame);
-           }
-       });
-        registrationButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                panelHome.setVisible(false);
-                new RegstrationPage(jFrame);
-            }
-        });
-        Exitbutton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                jFrame.dispose();
-            }
-        });
-        jFrame.setContentPane(new JLabel((new ImageIcon("src\\MainPackage\\images\\holla1.jpg"))));
-        jFrame.setSize(500,500);
+        Homelabel.setForeground(Color.WHITE);
+        aboutuslabel.setForeground(Color.WHITE);
+        policylabel.setForeground(Color.WHITE);
+        faqslabel.setForeground(Color.WHITE);
+        contactlabel.setForeground(Color.WHITE);
+        Hotellabel.setFont(new Font("Times New Roman",Font.BOLD,100));
+
+        textlabel.setFont(new Font("Times New Roman",Font.ITALIC,20));
+        hollalabel.setFont(new Font("Times New Roman",Font.BOLD,100));
+
+        textlabel.setBounds(340,380,1000,25);
+        hollalabel.setBounds(680,100,800,400);
+        Hotellabel.setBounds(300,100,800,400);
+        Homelabel.setBounds(800,120,50,40);
+        aboutuslabel.setBounds(850,120,70,40);
+        policylabel.setBounds(920,120,50,40);
+        faqslabel.setBounds(970,120,50,40);
+        contactlabel.setBounds(1020,120,50,40);
+        loginbutton.setBounds(550,450,100,40);
+        // contactlabel.setBounds(850,120,50,30);
+
+        loginbutton.setForeground(Color.WHITE);
+        loginbutton.setBackground(Color.RED);
+        //  panelMiddle.add(loginbutton);
+
+
+        textlabel.setForeground(Color.WHITE);
+        hollalabel.setForeground(Color.WHITE);
+        Hotellabel.setForeground(Color.red);
+
+        panelMiddle.add(textlabel);
+        panelMiddle.add(hollalabel);
+        panelMiddle.add(Homelabel);
+        panelMiddle.add(aboutuslabel);
+        panelMiddle.add(policylabel);
+        panelMiddle.add(faqslabel);
+        panelMiddle.add(contactlabel);
+
+        panelMiddle.add(Hotellabel);
+        panelMiddle.add(loginbutton);
+
+        //jFrame.add(panelHome);
+        jFrame.add(panelMiddle);
+        jFrame.setSize(1200, 680);
         jFrame.setVisible(true);
         jFrame.setResizable(false);
     }
+
+
 }
---
+++ src/OwnerLogin.java
@@ -0,0 +1,62 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by jatin kumar vimal on 03/07/2017.
+ */
+public class OwnerLogin {
+    JLabel ViewLabel;
+    JButton nextbutton,exitbutton;
+    JComboBox<String> StaffOption;
+    JPanel AdminPanel;
+    String[] Option = new String[]{"Select....", "Manager", "Employee", "Others"};
+    public OwnerLogin(JFrame jFrame) {
+        jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
+        jFrame.setTitle("Admin Login");
+        jFrame.setSize(1000, 1000);
+        jFrame.setResizable(false);
+
+        ViewLabel=new JLabel("VIEW");
+        nextbutton= new JButton("NEXT");
+        exitbutton= new JButton("EXIT");
+        StaffOption = new JComboBox<>(Option);
+
+        AdminPanel= new JPanel();
+        AdminPanel.setLayout(null);
+        jFrame.add(AdminPanel);
+
+        ViewLabel.setBounds(100,200,200,25);
+        nextbutton.setBounds(300,300,100,25);
+        exitbutton.setBounds(200,300,100,25);
+        StaffOption.setBounds(200,200,200,25);
+
+
+        AdminPanel.add(ViewLabel);
+        AdminPanel.add(StaffOption);
+        AdminPanel.add(nextbutton);
+        AdminPanel.add(exitbutton);
+
+        jFrame.setVisible(true);
+        jFrame.setResizable(false);
+        exitbutton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jFrame.dispose();
+            }
+        });
+
+        nextbutton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                AdminPanel.setVisible(false);
+                new SelectRoom(jFrame);
+            }
+        });
+
+
+
+
+
+    }
+}
